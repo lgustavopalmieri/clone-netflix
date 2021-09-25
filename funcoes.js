fcd4bb798921851b94d47171bee37febb3c26f9c
lgustavopalmieri <lgustavopalmieri@gmail.com>
2021-09-25 19:53:00 -0300

    finalizando

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+function assistir(){
+    let assistiragora = document.getElementById("assista");
+    window.open("https://www.youtube.com/watch?v=8QnMmpfKWvo")
+}
+
+
+
+function informar(){
+    let informacoes = document.getElementById("informe");
+    window.open("https://www.themoviedb.org/tv/1425-house-of-cards")
+}
+
+
+
+
